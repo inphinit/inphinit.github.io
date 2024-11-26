06269aee9b249e709460f8e619bbaf75d588bb4c
Guilherme Nascimento <brcontainer@yahoo.com.br>
2024-11-26 16:46:55 -0300

    Update

---
+++ js/docs.1732650226469.js
@@ -0,0 +1,58 @@
+{
+  function isSystemDark() {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
+
+  const root = document.documentElement;
+  const currentColorScheme = localStorage.getItem('color-scheme');
+
+  switch (localStorage.getItem('color-scheme')) {
+    case 'dark':
+      root.classList.toggle('dark', true);
+      break;
+    case 'light':
+      // Nothing
+      break;
+    default:
+      if (isSystemDark()) {
+        root.classList.toggle('dark', true);
+      }
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const colorScheme = document.querySelector('#color-scheme select');
+    const menuBackdrop = document.getElementById('menu-backdrop');
+    const menuToggle = document.getElementById('menu-toggle');
+    const body = document.body;
+
+    switch (currentColorScheme) {
+      case 'auto':
+      case 'dark':
+      case 'light':
+        colorScheme.value = currentColorScheme;
+        break;
+    }
+
+    colorScheme.addEventListener('change', () => {
+      const value = colorScheme.value.trim().toLowerCase();
+      let isDark = false;
+
+      if (value === 'auto') {
+        isDark = isSystemDark();
+      } else {
+        isDark = value === 'dark';
+      }
+
+      root.classList.toggle('dark', isDark);
+      localStorage.setItem('color-scheme', value);
+    });
+
+    menuToggle.addEventListener('click', () => {
+      body.classList.toggle('show-menu');
+    });
+
+    menuBackdrop.addEventListener('click', () => {
+      body.classList.toggle('show-menu', false);
+    });
+  });
+}
